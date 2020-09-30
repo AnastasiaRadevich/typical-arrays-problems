b499e721b81d36817b013499774ba6d1b39b68d3
Radevich <n.radievich@mail.ru>
2020-09-30 19:01:10 +0300

    feat: add task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (Array.isArray(array)) {
+    let arr = array.sort(function (a, b) {
+      return a - b;
+    })
+    if (arr.length > 0) {
+      return arr[0]
+    }
+  }
+  return 0;
+
+}
+
+
+
+exports.max = function max(array) {
+  if (Array.isArray(array)) {
+    let arr = array.sort((a, b) => {
+      return a - b;
+    })
+    if (arr.length > 0) {
+      return arr[arr.length - 1]
+    }
+  }
   return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (Array.isArray(array) && array.length > 0) {
+    let result = array.reduce((sum, cur) => {
+      return sum + cur;
+    })
+    return result / array.length;
+  }
   return 0;
 }
